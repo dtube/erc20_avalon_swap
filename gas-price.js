4aa04905e1bef57fe2a4360144075e3be833e434
Adrien M <adrien@d.tube>
2020-10-29 18:14:55 +0100

    fix another bug

--- gas-price.js
+++ gas-price.js
@@ -4,11 +4,11 @@ var web3 = new Web3('https://mainnet.infura.io/v3/'+config.infuraKey);
 module.exports = function(cb) {
     web3.eth.getGasPrice(function(err, res) {
         if (err) throw err
+        if (cb) cb()
         if (res != ethGasPrice) {
             ethGasPrice = res
             console.log('Gas Price: '+Math.round(ethGasPrice/1000000000)+' Gwei')
             updateFee()
-            if (cb) cb()
         }
     })
 }
